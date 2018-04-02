4eb5ac0c56413ab1af42e539bf3a9940c4d24973
Amanda Wray <amandawray2010@gmail.com>
2018-04-02 14:24:36 -0600

    added new routes to offer.js

---
+++ routes/offer.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+// get all offers
+router.get('/', function(req, res, next) {
+  res.send('Get an offer')
+});
+
+//get a single offers
+router.get('/:id', function(req, res, next) {
+  res.send('Get a single offer');
+});
+
+//post a new offer
+router.post('/', function(req, res, next){
+  res.send('Post new offer')
+})
+
+//delete an offer
+router.delete('/:id', function(req, res, next){
+  res.send('Delete an offer')
+})
+
+//update an offer
+router.patch('/:id', function(req, res, next){
+  res.send('Update an offer')
+})
+
+
+module.exports = router;
